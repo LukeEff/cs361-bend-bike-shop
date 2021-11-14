22bb685a614fd7e927f94ed53a7ebafd51fcdfdd
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2021-11-13 19:47:54 -0800

    Refactor bike.rb
    bike.rb was doing more than one thing, so the code related to pannier was moved into its own class. The code within the main function was violating the law of demeter, so the refactor makes it so that is no longer occuring and enforces the SRP.

--- main.rb
+++ main.rb
@@ -2,12 +2,13 @@
 
 require_relative 'bike'
 
-bike = Bike.new(1, :pink, 99.99)
+pannier = Pannier.new()
+pannier.add_cargo(:apples)
+pannier.add_cargo(:water)
+pannier.add_cargo(:repair_kit)
 
-bike.add_cargo(:apples)
-bike.add_cargo(:water)
-bike.add_cargo(:repair_kit)
+bike = Bike.new(1, :pink, 99.99, pannier: pannier)
 
-puts "Space for #{bike.pannier_remaining_capacity} items left."
+puts "Space for #{pannier.remaining_capacity} items left."
 
 bike.rent!
